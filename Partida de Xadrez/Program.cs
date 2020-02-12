bb30b379ee7479696dcbf4637a996ac6642d4e17
enzobonomi <enzo.bonomi@outlook.com>
2020-02-12 10:06:12 -0300

    Projeto iniciado

---
+++ Partida de Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Partida_de_Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
