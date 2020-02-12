a0bb08debfb86390852a2f73064226732ecb27ae
enzobonomi <enzo.bonomi@outlook.com>
2020-02-12 12:38:16 -0300

    Criando a classe que calcula e converte a posicao da peca de xadrez na matriz

--- Partida de Xadrez/Program.cs
+++ Partida de Xadrez/Program.cs
@@ -8,20 +8,12 @@ namespace Partida_de_Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez posicao = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(posicao); ;
 
 
-                tab.colocarPeca(new Torre(tab, CorDaPeca.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Rei(tab, CorDaPeca.Preta), new Posicao(0, 2));
-                tab.colocarPeca(new Rei(tab, CorDaPeca.Preta), new Posicao(3, 9));
-                Tela.MostrarTabuleiro(tab);
-            }
-            catch (TabuleiroExeption error)
-            {
-                Console.WriteLine(error.Message);
-            }
+            Console.WriteLine(posicao.toPosicao());
         }
     }
 }
